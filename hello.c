5a2e30eebac8fe12d5810314d20943020ca90ab4
xupeipei <only0903030111>
2014-07-23 11:01:18 +0800

    modify hello

--- hello.c
+++ hello.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
+int fun(int a, int b)
+{
+	return a + b;
+}
+
 int main(int argc, char **argv)
 {
+	int a;
+	int b;
 	printf("hello github!\n");
+	printf("%d", fun(a + b));
 	return 0;
 }
